59b300e1f39f83db1ccd0517e20f640c3e2715f4
Vitor Morais <vitor.morais@sanar.com>
2022-11-21 19:57:11 -0300

    chore: configs migrations and creates products migration

---
+++ src/app/configurations/migrations/typeorm/products.migration.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProducts1600016576988 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar(200)',
+          },
+          {
+            name: 'days_to_expiration',
+            type: 'int',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
