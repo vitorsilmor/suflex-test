c0b03612b29ebf90c8bcd77ee3e4936e8aaa5e53
Vitor Morais <vitorsilmor@gmail.com>
2022-11-23 21:01:11 -0300

    chore: removes unnecessary files from nestjs

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModules } from './configs';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './app/products/product.module';
 import { SeedService } from './configs/database/typeorm/seeds/seed.service';
 
 @Module({
   imports: [...ConfigurationModules, ProductsModule],
-  controllers: [AppController],
-  providers: [AppService, SeedService],
+  providers: [SeedService],
 })
 export class AppModule {
   constructor(private readonly seedingService: SeedService) {}
